652979eb6e7a65ff616f0691264b24d0201daf1a
FMY <Fengmayou@outlook.com>
2024-01-29 19:38:46 +0800

    4

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 int x;
+char q [100] = {'n','o'};
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "Hello world!" << std::endl << q[0] << q[1] << std::endl;
     std::cin.get();
 }
