625a2f113277ed0eaa52f358664ab772a9ed7f08
FMY <Fengmayou@outlook.com>
2024-01-29 19:42:07 +0800

    beta_0.4

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,6 @@ char q [100] = {'n','o'};
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello world!" << std::endl << q[0] << q[1] << std::endl;
+    std::cout << "beta_0.4" << std::endl << q[0] << q[1] << std::endl;
     std::cin.get();
 }
