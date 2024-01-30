dbfc2f1b44828547d5fa0672a392138fc71755f1
FMY <Fengmayou@outlook.com>
2024-01-30 13:55:48 +0800

    official release(fz)

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 //初始化
 int x;
-string beta/*版本号*/ = "0.4.1";
+string beta/*版本号*/ = "0.4.2";
 char q [100] = {'n','o'};
 //定义函数
 int main()
 {
-    cout << "beta_"
-         //<<"official release_"
+    cout //<< "beta_"
+         <<"official release_"
          << beta << endl << "IN:"<< q[0] << q[1] << endl;
     //esc
     cin.get();
