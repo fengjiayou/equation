a37a16faaefc208ce6836a7ad1b8001de5957236
FMY <Fengmayou@outlook.com>
2024-01-30 17:43:33 +0800

    0.4.2.1/R"(cin << )"

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 //初始化
 int x;
-string beta/*版本号*/ = "0.4.2";
-char q [100] = {'n','o'};
+string beta/*版本号*/ = "0.4.2.1";
+char q/*输入字符串*/ [100] = {'n','o'};
 //定义函数
 int main()
 {
     cout << "beta_"
          //<<"official release_"
-         << beta << endl << "IN:"<< q[0] << q[1] << endl;
+         << beta << endl << "IN:"<< q[0] << q[1] << endl << R"(cin << )";
+         cin.get(q,100).get();
     //esc
     cin.get();
     return 0;
