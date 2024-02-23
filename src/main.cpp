337e4644eb9358fad7964aa3548c3687ab296d3a
FMY <Fengmayou@outlook.com>
2024-02-23 21:22:02 +0800

    0.4.2.3

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,22 @@
 #include <string>
 using namespace std;
 //初始化
-int x [1];
-string beta/*版本号*/ = "0.4.2.1";
+string x [1000];
+int y [1000];
+string beta/*版本号*/ = "0.4.2.3";
 //定义函数
 int main()
 {
     cout << "beta_"
          //<<"official release_"
-         << beta << endl;
+         << beta << endl
          /*___________________*/
+         << R"(请空格输入: eg : 23 + 32 = x)" << endl;
+         /*___________________*/
+    for (int i = 1; i < 0; i++)
+    {
+        
+    }
     //esc
     cin.get();
     return 0;
